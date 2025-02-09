41922b70e868055138f782a862244264c9399c32
RomanticGuy <109668122+codeTravelerLee@users.noreply.github.com>
2025-02-09 05:09:16 +0000

    서버 컨트롤러 작성(폼 필드만 지정)

--- file-uploader/backend/file-uploader-server/src/app.controller.ts
+++ file-uploader/backend/file-uploader-server/src/app.controller.ts
@@ -1,12 +1,20 @@
-import { Controller, Get } from '@nestjs/common';
+import {
+  Controller,
+  Post,
+  UploadedFile,
+  UseInterceptors,
+} from '@nestjs/common';
 import { AppService } from './app.service';
+import { FileInterceptor } from '@nestjs/platform-express';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @Post('file-upload')
+  @UseInterceptors(FileInterceptor('file'))
+  uploadFile(@UploadedFile() file: Express.Multer.File) {
+    console.log(file.buffer.toString('utf-8'));
+    return 'your file is successfully uploaded.';
   }
 }
